8744b007cc46602a0b0cfea4d7fdbea3d288cecf
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-07 20:46:19 +0000

    add files for structure and style the header section

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,8 @@
 const APIkey = "074b0008c9908e74560eb4ed254c480c";
+
+async function getWeaterData() {
+    const res = await $.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIkey}`)
+    console.log(res);
+}
+
+getWeaterData();
