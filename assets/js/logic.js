38a5b87a477b729ecca37d3f45ebc13f021f4159
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-08 17:06:02 +0000

    document all functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,10 +1,14 @@
+// the api key
 const APIkey = "074b0008c9908e74560eb4ed254c480c";
 
+// render the icon associated with this weather condition
 async function renderWeatherIcon(iconCode, iconTag) {
     let src = "http://openweathermap.org/img/w/" + iconCode + ".png";
     iconTag.attr('src', src);
 }
 
+
+// get the weather condition of the city from the openweather api
 async function getWeaterData() {
     let res = await $.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIkey}`)
     res = await $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${res.coord.lat}&lon=${res.coord.lon}&appid=${APIkey}`)
