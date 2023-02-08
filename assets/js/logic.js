12a84300a36e86440111fac1a2a07298722c6d0c
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-08 20:47:27 +0000

    write functions to get and retrive relevant data

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -12,9 +12,27 @@ async function renderWeatherIcon(iconCode, iconTag) {
 async function getWeaterData() {
     let res = await $.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIkey}`)
     res = await $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${res.coord.lat}&lon=${res.coord.lon}&appid=${APIkey}`)
-    console.log(res);
-    let cod = res.list[0].weather[0].icon
-    renderWeatherIcon(cod, $('#wicon'));
+    return res;
 }
 
-getWeaterData();
+async function extractTodayAndFiveDayData() {
+    let now = moment().format('YYYY-MM-DD')
+    const data = await getWeaterData();
+    console.log(data)
+    const relevantData = [];
+    let currentDataDate = data.list[0].dt_txt.split(" ")[0];
+    let counter = 1;
+    for (let i = 0; i < data.list.length; i++) {
+        if (counter == 7) break;
+        if (now === currentDataDate) {
+            relevantData.push(data.list[i]);
+            now = moment().add(counter, 'days').format('YYYY-MM-DD');
+            console.log(currentDataDate);
+            counter++;
+        }
+        currentDataDate = data.list[i].dt_txt.split(" ")[0];
+    }
+    return relevantData
+}
+
+extractTodayAndFiveDayData().then((res) => console.log(res));
